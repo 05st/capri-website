b51efdc933dfd577b57f6333eaa06051e0add5a1
05st <stimsina@protonmail.com>
2022-07-15 13:02:36 -0500

    Deploy website - based on 4cd95bd9b7912e79eb2791f994f988e667079817

---
+++ assets/js/636f6651.badeff78.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcapri_website=self.webpackChunkcapri_website||[]).push([[418],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
