4cd95bd9b7912e79eb2791f994f988e667079817
05st <stimsina@protonmail.com>
2022-07-15 13:00:36 -0500

    Set config for github pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Capri Docs',
   tagline: 'Capri language documentation',
-  url: '05st.github.io',
-  baseUrl: '/',
+  url: 'https://05st.github.io',
+  baseUrl: '/capri-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,8 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: '05st', // Usually your GitHub org/user name.
-  projectName: 'capri', // Usually your repo name.
+  projectName: 'capri-website', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
